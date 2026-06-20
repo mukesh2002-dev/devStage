023841760827d0f0a4e0bada78bb8d84c4dba1c7
Mukes Kumar <156907997+mukesh2002-dev@users.noreply.github.com>
2026-06-20 07:07:32 +0530

    Create Test123.java

---
+++ Test123.java
@@ -0,0 +1,12 @@
+public class Test123 {
+
+    public static void main(String[] args) {
+
+        float first = 1.5f;
+        float second = 2.0f;
+
+        float product = first * second;
+
+        System.out.println("The test product is: " + product);
+    }
+}
